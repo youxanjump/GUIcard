c787b28395b9d9d4d4faec9e3628063381b7cafc
北市大Angelababy <nancy@GIGIteki-MacBook-Air.local>
2017-01-07 13:46:43 +0800

    delete some layouts and add new title

--- app/src/main/java/com/example/user/guicard/MainActivity.java
+++ app/src/main/java/com/example/user/guicard/MainActivity.java
@@ -12,12 +12,11 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity);
+        setContentView(R.layout.activity_title);
 
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/surf.ttf");
         TextView GUI = (TextView) findViewById(R.id.GUI);
         TextView Card = (TextView) findViewById(R.id.Card);
-        Button MP = (Button) findViewById(R.id.MP);
 
         //修改字體
         GUI.setTypeface(font);
